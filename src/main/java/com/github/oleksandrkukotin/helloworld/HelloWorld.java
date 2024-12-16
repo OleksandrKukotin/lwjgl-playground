9db1c3c06ffe95835f67d56350b46a958aaedee9
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-12-16 23:36:13 +0200

    Created a fine triangle

--- src/main/java/com/github/oleksandrkukotin/helloworld/HelloWorld.java
+++ src/main/java/com/github/oleksandrkukotin/helloworld/HelloWorld.java
@@ -95,12 +95,18 @@ public class HelloWorld {
         GL.createCapabilities();
 
         // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            glBegin(GL_TRIANGLES);
+            glColor3f(1.0f, 0.0f, 0.0f); glVertex2f(-0.5f, -0.5f);
+            glColor3f(0.0f, 1.0f, 0.0f); glVertex2f(0.5f, -0.5f);
+            glColor3f(0.0f, 0.0f, 1.0f); glVertex2f(0.0f, 0.5f);
+            glEnd();
 
             glfwSwapBuffers(window); // swap the color buffers
 
