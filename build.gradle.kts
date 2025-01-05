742467fd401847239702bdadf457fd634e969239
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-01-05 22:12:07 +0200

    Got rid of useless dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,14 +11,11 @@ repositories {
 
 val lwjglCoreVersion = "3.3.0"
 val lwjglBindingsVersion = "3.3.0"
-val ffMpegVersion = "7.0.1-1.2"
 
 dependencies {
     implementation("org.lwjgl:lwjgl:$lwjglCoreVersion")
     implementation("org.lwjgl.osgi:org.lwjgl.glfw:$lwjglBindingsVersion")
     implementation("org.lwjgl.osgi:org.lwjgl.opengl:$lwjglBindingsVersion")
-
-    implementation("com.nextbreakpoint:com.nextbreakpoint.ffmpeg4java:$ffMpegVersion")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
