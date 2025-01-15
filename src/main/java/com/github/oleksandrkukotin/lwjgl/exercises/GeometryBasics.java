b017043e2c19a6eae74835eac1e18efee2ced13a
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-01-15 23:37:21 +0200

    Refactored GeometryBasics code

--- src/main/java/com/github/oleksandrkukotin/lwjgl/exercises/GeometryBasics.java
+++ src/main/java/com/github/oleksandrkukotin/lwjgl/exercises/GeometryBasics.java
@@ -6,6 +6,7 @@ import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GLUtil;
 import org.lwjgl.system.Callback;
 import org.lwjgl.system.MemoryStack;
 
@@ -15,15 +16,14 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL15C.glGenBuffers;
-import static org.lwjgl.system.MemoryUtil.*;
 import static org.lwjgl.system.MemoryUtil.memAddress;
 
 public class GeometryBasics {
 
     private long window;
-    private int width, height;
+    private int width;
+    private int height;
 
-    private GLFWErrorCallback errorCallback;
     private GLFWKeyCallback keyCallback;
     private GLFWFramebufferSizeCallback fbCallback;
     private Callback debugProc;
@@ -31,9 +31,8 @@ public class GeometryBasics {
     public void run() {
         try {
             initializeGLFW();
-            setupAndInitializeOpenGLContext();
-            createWindow();
             render();
+
             glfwDestroyWindow(window);
             keyCallback.free();
             fbCallback.free();
@@ -49,33 +48,10 @@ public class GeometryBasics {
         if (!glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
-        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
 
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-        window = glfwCreateWindow(1920/2, 1080/2, "Hello World!", NULL, NULL);
-        if (window == NULL) {
-            throw new RuntimeException("Failed to create the GLFW window");
-        }
-
-        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
-            @Override
-            public void invoke(long window, int key, int scancode, int action, int mods) {
-                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                    glfwSetWindowShouldClose(window, true);
-            }
-        });
-        glfwSetFramebufferSizeCallback(window, fbCallback = new GLFWFramebufferSizeCallback() {
-            @Override
-            public void invoke(long window, int w, int h) {
-                if (w > 0 && h > 0) {
-                    width = w;
-                    height = h;
-                }
-            }
-        });
+        setHints();
+        createWindow();
+        setCallbacks();
 
         GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         glfwSetWindowPos(window, (vidMode.width() - width) / 2, (vidMode.height() - height) / 2);
@@ -85,50 +61,92 @@ public class GeometryBasics {
             width = framebufferSize.get(0);
             height = framebufferSize.get(1);
         }
-        glfwMakeContextCurrent(window);
-        glfwSwapInterval(1);
-        glfwShowWindow(window);
+        setupAndInitializeOpenGLContext();
+    }
+
+    private void setHints() {
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
     }
 
     private void createWindow() {
-        window = glfwCreateWindow(1920/2, 1080/2, "Geometry Basics", 0, 0);
+        window = glfwCreateWindow(1920 / 2, 1080 / 2, "Geometry Basics", 0, 0);
         if (window == 0) {
             throw new IllegalStateException("Unable to create window");
         }
+    }
+
+    private void setCallbacks() {
+        GLFWErrorCallback errorCallback = GLFWErrorCallback.createPrint(System.err);
+        glfwSetErrorCallback(errorCallback);
+
+        keyCallback = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                    glfwSetWindowShouldClose(window, true);
+            }
+        };
+        glfwSetKeyCallback(window, keyCallback);
+
+        fbCallback = new GLFWFramebufferSizeCallback() {
+            @Override
+            public void invoke(long window, int w, int h) {
+                if (w > 0 && h > 0) {
+                    width = w;
+                    height = h;
+                }
+            }
+        };
+        glfwSetFramebufferSizeCallback(window, fbCallback);
     }
 
     private void setupAndInitializeOpenGLContext() {
         glfwMakeContextCurrent(window);
         glfwSwapInterval(1);
         glfwShowWindow(window);
-        GL.createCapabilities();
     }
 
     private void render() {
+
+        GL.createCapabilities();
+        debugProc = GLUtil.setupDebugMessageCallback();
+
+        glClearColor(1.0f, 0.3f, 0.3f, 0.0f);
+
         int vbo = glGenBuffers();
         int ibo = glGenBuffers();
-        float[] vertices = {
-                0.1f, 0.0f, 0.0f,
-                -0.1f, 0.0f, 0.0f,
-                0.0f, 0.5f, 0.0f,
-        };
 
+        float[] vertices = {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f};
+        int[] indices = {0, 1, 2};
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, BufferUtils.createFloatBuffer(vertices.length).put(vertices).flip(), GL_STATIC_DRAW);
         glEnableClientState(GL_VERTEX_ARRAY);
+
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, BufferUtils.createIntBuffer(indices.length).put(indices).flip(), GL_STATIC_DRAW);
         glVertexPointer(2, GL_FLOAT, 0, 0L);
 
+        float color = 0.0f;
         while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            glViewport(0, 0, width, height);
+            glMatrixMode(GL_PROJECTION);
+            float aspect = (float) width / height;
+            glLoadIdentity();
+            glOrtho(-aspect, aspect, -1, 1, -1, 1);
+            glColor3f(1.0f, Math.abs((float) Math.tan(color)), 0.0f);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0L);
+
+            glfwSwapBuffers(window); // swap the color buffers
+
             glfwPollEvents();
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            glColor3f(1.0f, 1.0f, 0.5f);
-
-            glEnableClientState(GL_VERTEX_ARRAY);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-            glDisableClientState(GL_VERTEX_ARRAY);
-
-            glfwSwapBuffers(window);
+            color += 0.02f;
         }
     }
 
