ff05ce2c7a007881bfb0284b68fe6cfec450dff5
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-01-21 22:30:56 +0200

    Added new repos and fixed the GLM dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://maven.scijava.org/content/repositories/public/")
 }
 
 val lwjglCoreVersion = "3.3.0"
 val lwjglBindingsVersion = "3.3.0"
+val glmVersion = "1.0.1"
 
 dependencies {
     implementation("org.lwjgl:lwjgl:$lwjglCoreVersion")
     implementation("org.lwjgl.osgi:org.lwjgl.glfw:$lwjglBindingsVersion")
     implementation("org.lwjgl.osgi:org.lwjgl.opengl:$lwjglBindingsVersion")
 
-    implementation("io.github.kotlin-graphics:glm:0.9.9.1-12")
+    implementation("com.github.kotlin-graphics:glm:$glmVersion")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
